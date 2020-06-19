fd7df451b9a4171bc6d78baba42699143b851abc
zcg <925137875@qq.com>
2020-06-19 09:30:15 +0800

    第二次

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 //配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+	config.headers.authorization = window.sessionStorage.getItem('token');
+//	固定写法,必须return config
+	return config;
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,18 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/User.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   	{path:'/', redirect:'/login'},
 		{path:'/login', component: Login},
-		{path:'/home', component: Home}
+		{path:'/home', 
+				component: Home, 
+				redirect: '/welcome',
+				children:[{path:'/welcome', component: Welcome}, {path:'/users', component: Users}]}
 ]
 
 const router = new VueRouter({
@@ -25,5 +30,11 @@ router.beforeEach((to, from, next) => {
 	}
 	next();
 })
+
+const originalPush = VueRouter.prototype.push
+   VueRouter.prototype.push = function push(location) {
+   return originalPush.call(this, location).catch(err => err)
+}
+
 
 export default router
